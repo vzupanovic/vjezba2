91c3904a3d1088b06c07a6e49922d1d6f3a6b2d8
evanzup <vanessa.zupanovic@ericsson.com>
2014-09-08 13:37:06 +0200

    mars

---
+++ prvi.tcl
@@ -0,0 +1,11 @@
+puts "Ja nemam pola glave" 
+puts {Ja nemam pola glave}
+puts "Ja nemam pola glave";
+puts "Ja nemam pola glave sa komentarom"; #komentar
+set X "kae z glavom"
+set Y "trolollolo"
+puts "$X odgovor: $Y";
+set a 23
+set b 14
+set y [expr {$a + $b}]
+puts "zbroj je: $y"
