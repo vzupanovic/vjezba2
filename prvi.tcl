6a0c21eb8c2b2b151c25e1fdd96567604b5107db
evanzup <vanessa.zupanovic@ericsson.com>
2014-09-08 14:19:17 +0200

    opet master

--- prvi.tcl
+++ prvi.tcl
@@ -9,3 +9,9 @@ set a 23
 set b 14
 set y [expr {$a + $b}]
 puts "zbroj je: $y"
+set x abc
+puts "Ovo je kao neka zamjena ili nesto: $x \n";
+set K {[set x "Ovo je string u stringu"]}
+puts "$K"
+set moja_varijabla [expr {$a>20 ? ($a+1) : ($a-1)}]
+puts "vrijednost je: $moja_varijabla";
